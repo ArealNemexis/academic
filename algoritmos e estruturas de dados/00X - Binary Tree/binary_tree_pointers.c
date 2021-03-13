66bcbd8d1d472ca7d7c027581dc405077f045cdc
Lucas Ferreira <lucas0409@gmail.com>
2021-03-13 15:31:16 -0300

    insertion pointer binary tree

--- algoritmos e estruturas de dados/00X - Binary Tree/binary_tree_pointers.c
+++ algoritmos e estruturas de dados/00X - Binary Tree/binary_tree_pointers.c
@@ -18,11 +18,50 @@ struct Node * newNode(int value){
     return new;
 }
 
+void insert(struct Node* root, int value){
+    struct Node* prev;
+    struct Node* actual = root;
+    
+    prev = NULL;
+    
+
+    while(actual){
+        prev = actual;
+
+        if(actual->value == value){
+            return;
+        }
+        else if(value < actual->value){
+            actual = actual->left;
+        }
+        else{
+            actual = actual->right;
+        }
+    }
+
+    struct Node * new = newNode(value);
+    
+    if(!root){
+        root = new;
+    }
+    else if(value < prev->value){
+        prev->left = new;
+    }else{
+        prev->right = new;
+    }
+}
+
 int main(int argc, char const *argv[])
 {
     struct Node * root;
     root = newNode(50);
     
+    insert(root, 49);
+    insert(root, 51);
+
+    printf("\t%d\n", root->value);
+    printf("%d", root->left->value);
+    printf("\t\t%d\n", root->right->value);
     return 0;
 }
 
