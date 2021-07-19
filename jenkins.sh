8a4505a8bdd545e900c492fbdd98e16c66025790
Lucas Ferreira <lucas0409@gmail.com>
2021-07-19 14:39:51 -0300

    jenkis install

---
+++ jenkins.sh
@@ -0,0 +1,4 @@
+wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
+sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+sudo apt update
+sudo apt install jenkins
